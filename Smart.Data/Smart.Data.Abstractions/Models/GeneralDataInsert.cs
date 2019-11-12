425f4078b1661e5a3ba0035647a2fe1ec53f344e
Masoud Tahmasebi <mtahmasebi118@gmail.com>
2019-11-12 18:47:15 +0330

    insert

---
+++ Smart.Data/Smart.Data.Abstractions/Models/GeneralDataInsert.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Collections.Generic;
+
+namespace Smart.Data.Abstractions.Models
+{
+    public class GeneralDataInsert
+    {
+        public string FieldName { get; set; }
+        public string Value { get; set; }
+    }
+    public class GeneralInsert
+    {
+        public List<GeneralDataInsert> List { get; set; } = new List<GeneralDataInsert>();
+    }
+
+}
--- Smart.Data/Smart.Data.Module/Contexts/CrudGeneralContext.cs
+++ Smart.Data/Smart.Data.Module/Contexts/CrudGeneralContext.cs
@@ -3,7 +3,6 @@ using ACoreX.WebAPI;
 using Smart.Data.Abstractions.Contracts;
 using Smart.Data.Abstractions.Models;
 using System;
-using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -40,6 +39,7 @@ namespace Smart.Data.Module.Contexts
                      ).FirstOrDefault();
                     StringBuilder sb = new StringBuilder();
 
+                    //TODO: operator
                     sb.AppendFormat("DELETE FROM {0} WHERE {1} = @param ", entity.ENTT_SOURCE, column.ETFD_NAME);
                     DBParam p1 = new DBParam { Name = "@param", Value = model.Value };
                     var result = _Idata.Execute(sb.ToString(), commandType: System.Data.CommandType.Text, p1);
@@ -55,17 +55,39 @@ namespace Smart.Data.Module.Contexts
 
 
         [WebApi(Route = "api/{entityName}/insert", Authorized = false, Method = WebApiMethod.Post)]
-        public Task Insert(string entityName, GeneralDataModel model)
+        public Task Insert(string entityName, GeneralInsert model)
         {
             using (SqlConnection cnn = _Idata.OpenConnection())
             {
                 try
                 {
+                    Entity entity = _Idata.Query<Entity>(
+                       "SELECT TOP (1) * FROM [CRM].[ADM].[ADM_ENTITIES] where [ENTT_NAME] = @entityName",
+                       new DBParam() { Name = "@entityName", Value = entityName }
+                       ).FirstOrDefault();
+                    StringBuilder columns = new StringBuilder();
+                    StringBuilder values = new StringBuilder();
+                    foreach (var item in model.List)
+                    {
+                        var column = _Idata.Query<EntityFields>(
+                        string.Format(
+                            "SELECT TOP (1) *   FROM [CRM].[ADM].[ADM_ENTITY_FIELDS] where [ETFD_ALIAS] = @columnName and [ETFD_ENTT_ID] = '{0}'",
+                        entity.ENTT_ID.ToString()),
+                        new DBParam() { Name = "@columnName", Value = item.FieldName }
+                        ).FirstOrDefault();
+                        columns.Append(column.ETFD_NAME + ",");
+                    }
+                    foreach (var item in model.List)
+                    {
+                        values.Append("'"+item.Value + "'"+ ",");
+                    }
+                    --columns.Length;
+                    --values.Length;
                     StringBuilder sb = new StringBuilder();
 
-                    sb.AppendFormat("Insert Into {0} ({1}) VALUES @param", entityName, model.Columns);
-                    DBParam p1 = new DBParam { Name = "@param", Value = model.Values };
-                    var result = _Idata.Execute(sb.ToString(), commandType: System.Data.CommandType.Text, p1);
+                    sb.AppendFormat("Insert Into {0} ({1}) VALUES ({2})", entity.ENTT_SOURCE, columns, values);
+                    //DBParam p1 = new DBParam { Name = "@param", Value = model.Values };
+                    var result = _Idata.Execute(sb.ToString(), commandType: System.Data.CommandType.Text);
                     return Task.CompletedTask;
                 }
                 catch (Exception ex)
